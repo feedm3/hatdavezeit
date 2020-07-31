42905279b78ed3931bbd44a3f78ea6572ebb0d1f
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-07-31 09:11:06 +0200

    feat: add index page

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hat Dave Zeit?</title>
+        <meta name="description" content="nein! 🤬"/>
+      </Head>
+      <h1 style={{ display: 'grid', placeItems: 'center' }}>nein! 🤬</h1>
+    </>
+  );
+}
