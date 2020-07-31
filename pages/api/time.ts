b93e38afe8890b67d4141e0f8343950029e9a779
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2020-07-31 11:09:51 +0200

    feat: return now times and next time

--- pages/api/time.ts
+++ pages/api/time.ts
@@ -1,14 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
-import { isAfter, isBefore } from 'date-fns';
+import { PrismaClient, times } from '@prisma/client';
+import { addHours, isAfter, isBefore } from 'date-fns';
 
 const prisma = new PrismaClient();
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const now = new Date();
+interface TimeResponse {
+  now: times[];
+  next: times;
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<TimeResponse>,
+) => {
+  // the new date will return the utc date, thats why we have to add 2 hourse
+  // to be in cte time
+  const now = addHours(new Date(), 2);
   const nowIso = now.toISOString();
 
-  const times = await prisma.times.findMany({
+  const nowAndFutureTimes = await prisma.times.findMany({
     orderBy: {
       fromTime: 'desc',
     },
@@ -28,13 +38,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
-  const nowTimes = times.filter((time) => {
+  const nowTimes = nowAndFutureTimes.filter((time) => {
     return (
       isBefore(new Date(time.fromTime), now) &&
       isAfter(new Date(time.toTime), now)
     );
   });
 
+  const nextTimes = nowAndFutureTimes
+    .filter((time) => isAfter(new Date(time.fromTime), now))
+    .sort(
+      (timeA, timeB) =>
+        new Date(timeA.fromTime).getTime() - new Date(timeB.fromTime).getTime(),
+    );
+  const nextTime = nextTimes.length > 0 ? nextTimes[0] : null;
+
   res.statusCode = 200;
-  res.json(JSON.stringify(nowTimes));
+  res.json({
+    now: nowTimes,
+    next: nextTime,
+  });
 };
